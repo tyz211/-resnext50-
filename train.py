b5e663fa46e08d4117e0f5e346f187debd74de6e
tyz211 <122991154+tyz211@users.noreply.github.com>
2024-05-14 23:06:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,6 @@ from tqdm import tqdm
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
 root = "./datasets/car/train"
 transform = transforms.Compose([
     transforms.Resize(256),
@@ -20,47 +19,46 @@ transform = transforms.Compose([
 train_dataset = car_data.Car(root=root, transforms=transform)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
-
-model = resnext50_32x4d( num_classes=10)
+model = resnext50_32x4d(num_classes=10)
 model.to(device)
-
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-
 
 num_epochs = 100
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
-
+    correct = 0
+    total = 0
 
     progress_bar = tqdm(enumerate(train_loader), total=len(train_loader))
     for i, data in progress_bar:
-
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
 
-
         optimizer.zero_grad()
-
 
         outputs = model(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
-
         running_loss += loss.item()
 
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
 
-        progress_bar.set_description(f"Epoch {epoch + 1}/{num_epochs} Loss: {running_loss / (i + 1):.3f}")
+        progress_bar.set_description(f"Epoch {epoch + 1}/{num_epochs} Loss: {running_loss / (i + 1):.3f} | Acc: {100 * correct / total:.3f}%")
 
+    epoch_loss = running_loss / len(train_loader)
+    epoch_acc = 100 * correct / total
 
-    print(f'Epoch {epoch + 1} complete! Average Loss: {running_loss / len(train_loader):.3f}')
-    if running_loss/len(train_loader)<0.005:
+    print(f'Epoch {epoch + 1} complete! Average Loss: {epoch_loss:.3f} | Accuracy: {epoch_acc:.3f}%')
+
+    if epoch_loss < 0.005:
         break
-torch.save(model.state_dict(), "resnext50_model3.pth")
+
+torch.save(model.state_dict(), "resnext50_model.pth")
 print('Finished Training')
-
-
